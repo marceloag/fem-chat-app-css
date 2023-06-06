e0df3075cfe4d91985e759bb787ea7b0d6dee0cf
Marcelo Javier Aguila <marceloag@Mac-mini-de-Marcelo.local>
2023-06-06 12:16:56 -0300

    Finished Challenge

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -4,6 +4,14 @@ module.exports = {
 	theme: {
 		extend: {
 			colors: {
+// TEXT
+      paleviolet: "hsl(276, 100%, 81%)",
+      moderateviolet: "hsl(276, 55%, 52%)",
+      desaturateddarkviolet: "hsl(271, 15%, 43%)",
+      grayishblue: "hsl(206, 6%, 79%)",
+      verydarkdesaturatedviolet : "hsl(271, 36%, 24%)",
+      darkgreyishviolet: "hsl(270, 7%, 64%)",
+// Gradients
 				lightmagenta: "hsl(293, 100%, 63%)",
 				lightviolet: "hsl(264, 100%, 61%)",
 				lightgrayishviolet: "hsl(270, 20%, 96%)",
